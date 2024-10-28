9ec25e46834525a3e7c740b22428f1ec6729ea8f
Paolo Marcolli <paolo.marcolli@hotmail.com>
2024-10-28 16:10:05 +0100

    add other javascript lessons

--- 010_javascript_introduction-part-3/asset/js/jsButtonParagraph.js
+++
@@ -1,8 +0,0 @@
-
-/*const paragraph = document.querySelector('p.button-style');
-paragraph.addEventListener('click', updateName);
-
-function updateName(){
-    let name = prompt('Enter your name:');
-    paragraph.textContent = 'Player 1: ' + name;
-}*/
